916a3ef341b7a7c8330554637060914a3142a362
Tobias Jansson <tobias.jansson@gmail.com>
2018-12-01 10:36:20 +0100

    Dag 1. Del 2

--- 01.js
+++ 01.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var args = process.argv.slice(2);
 
 function read(file, callback) {
-    fs.readFile(file, 'utf8', function(err, data) {
+    fs.readFile(file, 'utf8', function (err, data) {
         if (err) {
             console.log(err);
         }
@@ -10,14 +10,23 @@ function read(file, callback) {
     });
 }
 
-read(args[0], function(data) {
+read(args[0], function (data) {
     var lines = data.split('\n');
     let freq = 0;
-    for(let l of lines) {
-        if(l.length == 0) continue;
-        f = parseInt(l);
-        freq += f;
+    let found = [];
+    let foundtwice = false;
+
+    while (!foundtwice) {
+        for (let l of lines) {
+            if (l.length == 0) continue;
+            f = parseInt(l);
+            freq += f;
+            if(found[freq]) {
+                foundtwice = true;
+                break;
+            }
+            found[freq] = true;
+        }
     }
     console.log(freq);
 });
-
