ef49d169b6e1dd2d2b570c0fffc4ad13eba1c002
Tobias Jansson <tobias.jansson@gmail.com>
2018-12-02 10:01:05 +0100

    Dag 2, del 1. Läste slarvig så kostade tid

--- 02.js
+++ 02.js
@@ -13,9 +13,34 @@ function read(file, callback) {
 read(args[0], function (data) {
     var lines = data.split('\n');
 
+    let twos = 0;
+    let threes = 0;
+
     for (let l of lines) {
         if (l.length == 0) continue;
+        let chars = l.split('');
+        let counts = [];
 
+        chars.forEach(c => {
+            if(counts[c] == undefined) {
+                counts[c] = 1;
+            } else {
+                counts[c] = counts[c] + 1;
+            }
+        });
+
+        let found2 = 0;
+        let found3 = 0;
+        Object.keys(counts).forEach(c => {
+            let count = counts[c];
+
+            if(count == 2) found2 = 1;
+            if(count == 3) found3 = 1;
+        })
+        twos += found2;
+        threes += found3;
     }
+
+    console.log(twos * threes);
 
 });
