ccfac63e27c5c231390e0397f7123a530e523b4e
Tobias Jansson <tobias.jansson@prevas.se>
2018-12-03 09:44:32 +0100

    Regexstrul + blandade x och y

--- 03.js
+++ 03.js
@@ -13,20 +13,54 @@ function read(file, callback) {
 read(args[0], function (data) {
     var lines = data.split('\n');
 
+    var fabric = [];
+    var overlap = 0;
+    var maxx = 0;
+    var maxy = 0;
     for (let l of lines) {
         if (l.length == 0) continue;
-        let match = l.match(/#(\d+) @ (\d+),(\d+): (\d+)x(\d)/);
+        let match = l.match(/#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/);
 
-        if(match.length == 5) {
-            let id = match[1];
-            let left = match[2];
-            let top = match[3];
-            let width = match[4];
-            let height = match[5];
+        if (match.length) {
+            let id = parseInt(match[1]);
+            let left = parseInt(match[2]);
+            let top = parseInt(match[3]);
+            let width = parseInt(match[4]);
+            let height = parseInt(match[5]);
 
-            
-
+            for (let y = 0; y < height; y++) {
+                for (let x = 0; x < width; x++) {
+                    if (!fabric[top + y]) fabric[top + y] = [];
+                    let node = fabric[top + y][x + left] || { id: id, x: left + x, y: top + y, count: 0 };
+                    node.count++;
+                    if(node.count == 2) overlap++;
+                    fabric[top + y][x + left] = node;
+                    if(top+y > maxy) maxy = top + y;
+                    if(left+x > maxx) maxx = left + x;
+                }
+            }
         }
     }
+
+    // Skriv ut
+    // for (let y = 0; y < maxy; y++) {
+    //     let row = '';
+    //     for (let x = 0; x < maxx; x++) {
+    //         if (fabric[y]) {
+    //             let node = fabric[y][x];
+    //             if (node) {
+    //                 if(node.count > 1) row += 'X';
+    //                 else row += node.id;
+    //             } else { 
+    //                 row += '.';
+    //             }
+    //         } else {
+    //             row += '.';
+    //         }
+    //     }
+    //     console.log(row);
+    // }
+
+    console.log(overlap);
 
 });
