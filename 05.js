bcae8b0f08ac65d3c2a110ea2bb63119876b8b5d
Tobias Jansson <tobias.jansson@prevas.se>
2018-12-05 11:57:40 +0100

    Del 2

--- 05.js
+++ 05.js
@@ -15,15 +15,14 @@ read(args[0], function (data) {
 
     for (let l of lines) {
         if (l.length == 0) continue;
-
         let react = '';
         let reactions = true;
-        while(reactions) {
+        while (reactions) {
             react = '';
             let i = 0;
-            while(i < l.length) {
-                if(Math.abs(l.charCodeAt(i) - l.charCodeAt(i+1)) == 32) {
-                    i+=2;
+            while (i < l.length) {
+                if (Math.abs(l.charCodeAt(i) - l.charCodeAt(i + 1)) == 32) {
+                    i += 2;
                 } else {
                     react += l[i++];
                 }
@@ -31,7 +30,35 @@ read(args[0], function (data) {
             reactions = react != l;
             l = react;
         }
-        console.log('Units left: ' + react.length);
+        console.log('Del 1. Units left: ' + react.length);
     }
 
+    // Del 2
+    for (let l of lines) {
+        if (l.length == 0) continue;
+        let units = 'abcdefghijklmnopqrstuvxywz';
+        let polymers = [];
+        const input = l;
+        for (const unit of units) {
+            let react = '';
+            l = input.split('').filter(c => c.toLowerCase() != unit).join('');
+
+            let reactions = true;
+            while (reactions) {
+                react = '';
+                let i = 0;
+                while (i < l.length) {
+                    if (Math.abs(l.charCodeAt(i) - l.charCodeAt(i + 1)) == 32) {
+                        i += 2;
+                    } else {
+                        react += l[i++];
+                    }
+                }
+                reactions = react != l;
+                l = react;
+            }
+            polymers.push(react.length);
+        }
+        console.log('Del 2. Kortast polymer: ' + Math.min(...polymers));
+    }
 });
