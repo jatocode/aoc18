7fe872719dd8642e618de86fac93a8b7c85d8278
Tobias Jansson <tobias.jansson@prevas.se>
2018-12-05 11:24:46 +0100

    5.1

--- 05.js
+++ 05.js
@@ -16,6 +16,22 @@ read(args[0], function (data) {
     for (let l of lines) {
         if (l.length == 0) continue;
 
+        let react = '';
+        let reactions = true;
+        while(reactions) {
+            react = '';
+            let i = 0;
+            while(i < l.length) {
+                if(Math.abs(l.charCodeAt(i) - l.charCodeAt(i+1)) == 32) {
+                    i+=2;
+                } else {
+                    react += l[i++];
+                }
+            }
+            reactions = react != l;
+            l = react;
+        }
+        console.log('Units left: ' + react.length);
     }
 
 });
