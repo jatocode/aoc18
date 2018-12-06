3e77e192a58012751f9d9d1e1ddcddc131c448cf
Tobias Jansson <tobias.jansson@gmail.com>
2018-12-07 00:56:14 +0100

    räknar fel

--- 06.js
+++ 06.js
@@ -29,28 +29,39 @@ read(args[0], function (data) {
         if (x > maxx) maxx = x;
         if (y > maxy) maxy = y;
     }
-
     for (const node of nodes) {
         calculateDistances(grid, node, maxx, maxy);
     }
+    console.log(nodes);
 
-    checkGrid(grid, maxx, maxy);
+    let ns = checkGrid(grid, maxx, maxy);
+    let nn = ns.map(x => {
+        console.log(x);
+        x == undefined?0:x});
+    let max = Math.max(...nn);
+    //console.log(ns);
+    console.log('Största ytan: ' + (max+1));
 });
 
 function calculateDistances(grid, node, sx, sy) {
     for (let y = 0; y <= sy + 1; y++) {
         for (let x = 0; x <= sx + 1; x++) {
-            if (!grid[`${x}${y}`]) grid[`${x}${y}`] = { id: -1, dist: [] };
+            if (!grid[`${x}${y}`]) grid[`${x}${y}`] = { id: -1, dist: [], finite: true };
             let dist = Math.abs(node.x - x) + Math.abs(node.y - y);
             grid[`${x}${y}`].dist[node.id] = dist;
         }
     }
 }
+
 function checkGrid(grid, sx, sy) {
-    for (let y = 0; y <= sy + 1; y++) {
+    let nodes = [];
+    for (let y = 0; y <= sy; y++) {
         let row = '';
-        for (let x = 0; x <= sx + 1; x++) {
+        for (let x = 0; x <= sx; x++) {
             const node = grid[`${x}${y}`];
+            if (y == 0 || y == sy) node.finite = false;
+            if (x == 0 || x == sx) node.finite = false;
+
             if (node.id > -1) {
                 row += String.fromCharCode(node.id + 65);
             } else {
@@ -59,11 +70,15 @@ function checkGrid(grid, sx, sy) {
                 if (single == true) {
                     let id = node.dist.findIndex(x => x == mindist);
                     row += String.fromCharCode(id + 97);
+                    if(node.finite) {
+                        nodes[id] = (nodes[id] || 0) + 1;
+                    }
                 } else {
                     row += '.';
                 }
             }
         }
-        console.log(row);
+       // console.log(row);
     }
+    return nodes;
 }
