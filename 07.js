1edc998dbece19f3caa49c026259183c7c45e0bd
Tobias Jansson <tobias.jansson@prevas.se>
2018-12-07 09:31:44 +0100

    Började på 07

--- 07.js
+++ 07.js
@@ -13,9 +13,24 @@ function read(file, callback) {
 read(args[0], function (data) {
     var lines = data.split('\n');
 
+    let steps = [];
     for (let l of lines) {
         if (l.length == 0) continue;
+        const m = l.match(/Step (.) must be finished before step (.) can begin/);
+        let before, after;
+        [first, next] = [ m[1], m[2] ];
 
+        if(!steps[first]) steps[first] = { children: [] };
+        steps[first].children.push(next);
     }
+    
+    let steps2 = Object.keys(steps).map(k => { return [k, steps[k]]});
 
+    let order = '';
+    while(steps2.length) {
+        const step = steps2.shift();
+        console.log(step);
+        order += step[0];
+    }
+    console.log(order);
 });
