1331fcb8bc32a656e630bfedcc2422f52141ab9a
Tobias Jansson <tobias.jansson@gmail.com>
2018-12-10 00:47:04 +0100

    08 räknar fel

--- 08.js
+++ 08.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var args = process.argv.slice(2);
+let meta = 0;
 
 function read(file, callback) {
     fs.readFile(file, 'utf8', function (err, data) {
@@ -15,7 +16,37 @@ read(args[0], function (data) {
 
     for (let l of lines) {
         if (l.length == 0) continue;
+        data = l.split(' ').map(x => +x);
+        let marker = 0;
+        header(data, 0);
+        
+        console.log(meta);
 
+        inp1 = data;
+        console.log(part1());
     }
 
 });
+
+function header(data, marker) {
+    let size = 0;
+    const cn = +data[marker];
+    const md = +data[marker + 1];
+    console.log({cn, md});
+
+    size += 2;
+
+    for(i=0;i<cn;i++) {
+        let cz = header(data, marker + size); 
+        size += cz;
+    }
+
+    for(let i=0; i < md; i++) {
+        //console.log('md = ' + data[marker + size + i]);
+        meta += +data[marker + size + i];
+        console.log(meta);
+    }
+    size += md;
+
+    return size;
+}
