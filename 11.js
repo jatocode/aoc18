efe1931c26d80023979041ffb5b6b78f3e227480
Tobias Jansson <tobias.jansson@prevas.se>
2018-12-11 13:38:26 +0100

    11.2. LÅNGSAM!

--- 11.js
+++ 11.js
@@ -1,23 +1,29 @@
 const serial = 9798;
 
 let maxpower = 0;
+let maxsize = 0;
 let max;
-for (let y = 1; y < 301; y++) {
-    for (let x = 1; x < 301; x++) {
-        const tp = power3x3(x,y);
-        if(tp > maxpower) {
-            maxpower = tp;
-            max = {x,y}
+
+for (let size = 0; size < 300; size++) { // Part 2
+    for (let y = 1; y < 301; y++) {
+        for (let x = 1; x < 301; x++) {
+            const tp = powerSquare(x, y, size);
+            if (tp > maxpower) {
+                maxpower = tp;
+                max = { x, y, size }
+            }
         }
     }
+    console.log(size);
 }
 print5x5(max.x, max.y);
-console.log(max);
+console.log(max.x + ',' + max.y + ',' + max.size);
 
-function power3x3(sx, sy) {
+// Bygg en grid som kommer ihåg och räknar med multiplar...men inte nu :D
+function powerSquare(sx, sy, size) {
     let tp = 0;
-    for (let y = sy; y < sy + 3; y++) {
-        for (let x = sx; x < sx + 3; x++) {
+    for (let y = sy; y < sy + size; y++) {
+        for (let x = sx; x < sx + size; x++) {
             const p = powerlevel(x, y);
             tp += p;
         }
