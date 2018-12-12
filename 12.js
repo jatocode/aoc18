876699f92a54247d3c8d55cfe0b64ccb3b4a4eff
Tobias Jansson <tobias.jansson@gmail.com>
2018-12-12 07:45:42 +0100

    BÃrjat med 12

---
+++ 12.js
@@ -0,0 +1,41 @@
+var fs = require('fs');
+var fs = require('fs');
+var args = process.argv.slice(2);
+
+//const initialstate = '####..##.##..##..#..###..#....#.######..###########.#...#.##..####.###.#.###.###..#.####..#.#..##..#';
+const initialstate = '#..#.#..##......###...###';
+
+function read(file, callback) {
+    fs.readFile(file, 'utf8', function (err, data) {
+        if (err) {
+            console.log(err);
+        }
+        callback(data);
+    });
+}
+
+read(args[0], function (data) {
+    var lines = data.split('\n');
+
+    const init = initialstate.split('');
+
+    console.log(init);
+    for (let l of lines) {
+        if (l.length == 0) continue;
+
+        // LLCRR => N
+        let L1 = l[0];
+        let L2 = l[2];
+        let R1 = l[3];
+        let R2 = l[3];
+        let N  = l[9];
+        
+        console.log({L1, L2, R1, R2, N});
+        for (let i = 0; i < init.length; i++) {
+            const C = init[i];
+
+            
+        }
+    }
+
+});
---
+++ 13.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+var fs = require('fs');
+var args = process.argv.slice(2);
+
+function read(file, callback) {
+    fs.readFile(file, 'utf8', function (err, data) {
+        if (err) {
+            console.log(err);
+        }
+        callback(data);
+    });
+}
+
+read(args[0], function (data) {
+    var lines = data.split('\n');
+
+    for (let l of lines) {
+        if (l.length == 0) continue;
+
+    }
+
+});
